fix: de-normalization b4 augmentation

augmentation.py
@@ -4,7 +4,6 @@ from config import FP_PRECISION
 DECIMAL_PRECISION = 5
 PARAM_PRECISION = 2
 
-
 def truncate(value, decimals):
     factor = 10 ** decimals
     return np.trunc(value * factor) / factor
@@ -17,37 +16,43 @@ def generate_params():
     # Translation parameters
     tx = int(truncate(np.random.uniform(20, 40), 0))
     ty = int(truncate(np.random.uniform(-30, -10), 0))
-    tz = int(truncate(np.random.uniform(-5, 5), 0))
     
     # Scaling parameters
     scale_x = truncate(np.float32(np.random.uniform(1.1, 1.3)), PARAM_PRECISION)
     scale_y = truncate(np.float32(np.random.uniform(1.1, 1.3)), PARAM_PRECISION)
-    scale_z = truncate(np.float32(np.random.uniform(0.9, 1.1)), PARAM_PRECISION)
     
-    return (shear_x, shear_y), (tx, ty, tz), (scale_x, scale_y, scale_z)
+    return (shear_x, shear_y), (tx, ty), (scale_x, scale_y)
 
 def shear_landmarks(landmarks, shear_params):
     shear_x, shear_y = shear_params
-    shear_matrix = np.array([[1, shear_x, 0], 
-                            [shear_y, 1, 0], 
-                            [0, 0, 1]], dtype=FP_PRECISION)
     
-    landmarks_sheared = landmarks @ shear_matrix.T
+    landmarks_xy = landmarks[:, :2]
+    shear_matrix = np.array([[1, shear_x], 
+                            [shear_y, 1]], dtype=FP_PRECISION)
+    
+    landmarks_sheared_xy = landmarks_xy @ shear_matrix.T
+    landmarks_sheared = landmarks.copy()
+    landmarks_sheared[:, :2] = landmarks_sheared_xy
     return truncate(landmarks_sheared, DECIMAL_PRECISION)
 
 def translate_landmarks(landmarks, trans_params):
-    tx, ty, tz = trans_params
-    translation_vector = np.array([tx, ty, tz], dtype=FP_PRECISION)
-    return truncate(landmarks + translation_vector, DECIMAL_PRECISION)
+    tx, ty = trans_params
+    
+    landmarks_translated = landmarks.copy()
+    landmarks_translated[:, 0] += tx
+    landmarks_translated[:, 1] += ty
+    return truncate(landmarks_translated, DECIMAL_PRECISION)
 
 def scale_landmarks(landmarks, scale_params):
-    scale_x, scale_y, scale_z = scale_params
-    scaling_matrix = np.array([scale_x, scale_y, scale_z], dtype=FP_PRECISION)
-    return truncate(landmarks * scaling_matrix, DECIMAL_PRECISION)
+    scale_x, scale_y = scale_params
+    
+    landmarks_scaled = landmarks.copy()
+    landmarks_scaled[:, 0] *= scale_x
+    landmarks_scaled[:, 1] *= scale_y
+    return truncate(landmarks_scaled, DECIMAL_PRECISION)
 
-def augment_skeleton_sequence(frame_landmarks):
+def augment_skeleton_sequence(frame_landmarks, shear_params, trans_params, scale_params):
     landmarks = np.array(frame_landmarks, dtype=FP_PRECISION)
-    shear_params, trans_params, scale_params = generate_params()
     
     shearing = shear_landmarks(landmarks, shear_params)
     translation = translate_landmarks(landmarks, trans_params)

run_augmentation.py
@@ -3,7 +3,8 @@ import json
 import numpy as np
 from collections import defaultdict
 from augmentation import (generate_params, shear_landmarks, 
-                        translate_landmarks, scale_landmarks)
+                        translate_landmarks, scale_landmarks, 
+                        denormalize_landmarks, normalize_landmarks, augment_skeleton_sequence)
 from utils.jsonl_utils import load_jsonl_gz, save_jsonl_gz
 from config import *
 
@@ -49,10 +50,15 @@ def augment_and_save(input_path, output_path):
                 
                 for frame in video:
                     frame = np.array(frame, dtype=np.float32)
+                    frame = denormalize_landmarks(frame)
                     
                     sheared_frame = shear_landmarks(frame, shear_params)
                     translated_frame = translate_landmarks(frame, trans_params)
                     scaled_frame = scale_landmarks(frame, scale_params)
+                    
+                    sheared_frame = normalize_landmarks(sheared_frame)
+                    translated_frame = normalize_landmarks(translated_frame)
+                    scaled_frame = normalize_landmarks(scaled_frame)
                     
                     shearing_video.append(sheared_frame.tolist())
                     translation_video.append(translated_frame.tolist())